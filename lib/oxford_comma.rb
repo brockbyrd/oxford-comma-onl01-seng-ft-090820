129c11822adb13cf4b8db42f39616a1d56627dad
Brock Byrd <brockbyrdd@gmail.com>
2020-09-14 17:15:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   elsif array.length > 2
-    "#{array[0..-2].join(", ")}, and #{array.last}"
+    "#{array[0..-2], and #{array.last.join(", ")}}"
   else
     array.join
   end
